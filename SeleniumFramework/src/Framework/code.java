ad658a6725de78769c41849ef080c81af62bf45c
roru65456 <145462466+roru65456@users.noreply.github.com>
2023-10-25 03:16:32 -0700

    Update code.java
    iookk

--- SeleniumFramework/src/Framework/code.java
+++ SeleniumFramework/src/Framework/code.java
@@ -2,13 +2,4 @@ package Framework;
 
 public class code {
 	
-	public static void Data() {
-	System.out.println("code modified by rohit");	
-	}
-	
-	public static void main(String[]args) {
-		System.out.println("Code push by saurabh");
-		
-	}
-
 }
